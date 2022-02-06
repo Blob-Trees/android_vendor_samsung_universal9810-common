becac1a8996104e49ffd842c2ec494718b989123
Dylan Neve <dylanneve1@gmail.com>
2022-02-06 14:29:50 +0000

    universal9810: media: Update Codecs from N970F

--- universal9810-common-vendor.mk
+++ universal9810-common-vendor.mk
@@ -166,10 +166,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libOMX.Exynos.AVC.Decoder \
     libOMX.Exynos.AVC.Encoder \
-    libOMX.Exynos.AVC.WFD.Encoder \
     libOMX.Exynos.HEVC.Decoder \
     libOMX.Exynos.HEVC.Encoder \
-    libOMX.Exynos.HEVC.WFD.Encoder \
     libOMX.Exynos.MPEG4.Decoder \
     libOMX.Exynos.MPEG4.Encoder \
     libOMX.Exynos.VP8.Decoder \
